fe6b7007254c924f66155bf9f5d8892c76cbb193
Dalton Khatri <khatridalton1235@gmail.com>
2026-04-12 13:19:56 +0545

    Added new page

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './pages/Home';
 import Projects from './pages/Projects';
+import CaseStudy from './pages/CaseStudy';
 import About from './pages/About';
 import Experience from './pages/Experience';
 import Gallery from './pages/Gallery';
@@ -19,6 +20,7 @@ export default function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="projects" element={<Projects />} />
+          <Route path="projects/:id" element={<CaseStudy />} />
           <Route path="about" element={<About />} />
           <Route path="experience" element={<Experience />} />
           <Route path="gallery" element={<Gallery />} />
